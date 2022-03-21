f60038d1d862b66e2344113b8a177a1f315b673e
HARIHARAN2131 <hariharish3110@gmail.com>
2022-03-21 07:46:02 +0530

    calculate the sum of first 10 natural number

---
+++ Question2/Main.java
@@ -0,0 +1,11 @@
+package Question2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Natural_number number = new Natural_number();
+       number.printNumber();
+	}
+
+}
---
+++ Question2/Natural_number.java
@@ -0,0 +1,12 @@
+package Question2;
+
+public class Natural_number {
+	void printNumber() {
+		int sum=0;
+		for(int i=1;i<=10;i++) {
+			sum+=i;
+		}
+		System.out.println("Sum of First 10 Natural number : "+sum);
+	}
+
+}
