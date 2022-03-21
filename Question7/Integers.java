a2035f46c813343e8e796f7e157628808b209cb9
HARIHARAN2131 <hariharish3110@gmail.com>
2022-03-21 07:53:58 +0530

    Print sum of the even and odd number

---
+++ Question7/Integers.java
@@ -0,0 +1,23 @@
+package Question7;
+
+import java.util.Scanner;
+
+public class Integers {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int number, integer, odd = 0, even = 0;
+		System.out.println("Enter the numbers of integers ");
+		number = scanner.nextInt();
+		for (int i = 1; i <= number; i++) {
+			System.out.println("integer " + i);
+			integer = scanner.nextInt();
+			if (integer % 2 != 0)
+				odd = odd + integer;
+			else
+				even = even + integer;
+		}
+		System.out.println("Sum of odd : " + odd);
+		System.out.println("Sum of even : " + even);
+		scanner.close();
+	}
+}
